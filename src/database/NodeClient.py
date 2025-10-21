ee40123fecf4ceb77da44164f1aa5bd92366df1b
Aidil Zamri <aeeezeee.code@gmail.com>
2025-10-22 05:10:18 +0800

    Code fix - updated naming convention

--- src/database/NodeClient.py
+++ src/database/NodeClient.py
@@ -4,7 +4,7 @@
 :brief: This module defines all important functions for interacting with the external database.
 """
 from src.structures.node import Node
-from src.database.protoclient import ClientDB as protoclient
+from src.database.ProtoClient import ClientDB as protoclient
 from pymongo import MongoClient
 
 class NodeDB(protoclient):
