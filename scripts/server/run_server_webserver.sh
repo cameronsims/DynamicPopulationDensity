15418eee016c9a5e09b371deabc9fb8b5843366d
Aidil Zamri <aeeezeee.code@gmail.com>
2025-10-31 15:43:36 +0800

    Added deployment scripts for webserver - Aidil

--- scripts/server/run_server_webserver.sh
+++ scripts/server/run_server_webserver.sh
@@ -1,1 +1,17 @@
 # TODO: ADD WEB SERVER COMMAND
+
+sudo cp /opt/DynamicePopulationDensity/scripts/node/ict302-webserver.service /etc/systemd/system/
+sudo cp /opt/DynamicePopulationDensity/scripts/node/ict302-bridgeserver.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo chmod +x /opt/DynamicPopulationDensity/scripts/node/start_all_webserver.sh
+
+sudo cd /opt/DynamicPopulationDensity/src/server/frontend/hive-matrix/
+sudo npm install
+
+sudo systemctl enable ict302-webserver.service
+sudo systemctl enable ict302-bridge.service
+sudo systemctl start ict302-webserver.service
+sudo systemctl start ict302-webserver.service
+
---
+++ scripts/server/start_all_webserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Running command 1..."
+npm run dev
+
+echo "Running command 2..."
+npm run tw:watch
